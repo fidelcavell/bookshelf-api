63335fc8b6412d2f358c0b8f2d83b47c63ae4b2d
VellxF <fidel.cavell@gmail.com>
2023-07-11 11:26:36 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addBookHandler,
+  getAllBooksHandler,
+  getBooksByIdHandler,
+  updateBooksByIdHandler,
+  deleteBooksByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBooksByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: updateBooksByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBooksByIdHandler,
+  },
+];
+
+module.exports = routes;
